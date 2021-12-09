2b5da6d04d6b710e371aa8630d69483318eb560d
christien1 <christien1@PF2Z2P22.wit.private>
2021-12-09 08:16:03 -0500

    Added Enemies TO level

--- src/FirstMap.java
+++ src/FirstMap.java
@@ -13,7 +13,10 @@ public class FirstMap extends Map{
 		//graphic.setX(0);
 		graphic.setY(yScroll);
 		scrollSp=5;
-		enemyForms=new EnemyFormation[] {new TriplePea(-6900,-6000),new TriplePea(-5800,-5700),new DualZipper(-5750,-5600)};
+		enemyForms=new EnemyFormation[] {new TriplePea(-6900,-6000),new TriplePea(-5800,-5700),new DualZipper(-5750,-5600),new DualZipper(-5500,-5450),
+		new TriplePea(-5450,-5000),new TriplePea(-5350,-5000),new TriplePea(-5250,-5000),new DualZipper(-5250,-5000),new DualZipper(-5000,-4000),
+		new TriplePea(-4000,-1000),new TriplePea(-3000,-1000),new TriplePea(-2000,-1000),new TriplePea(-1000,-1000),new TriplePea(-500,0),
+		new DualZipper(-3500,0),new DualZipper(-3200,0),new DualZipper(-3000,0),new DualZipper(-2500,0),new DualZipper(-1500,0)};
 	}
 
 	@Override
